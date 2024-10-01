8f7940b91734c3a97cebc1c3e72c7774f452c020
FilipFE23 <filip.bockarovski@gritacademy.se>
2024-10-01 15:42:08 +0200

    Klar

--- clock-app/src/App.js
+++ clock-app/src/App.js
@@ -1,21 +1,29 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Clock from './clock';
 
 function App() {
-  const date = new Date();
+  const [time, setTime] = useState({
+    hour: '00',
+    minute: '00',
+    second: '00'
+  })
 
-  let time = {}
-
-  time.hour = String(date.getHours()).padStart(2, '0');
-  time.minute = String(date.getMinutes()).padStart(2, '0');
-  time.second = String(date.getSeconds()).padStart(2, '0');
+  useEffect(() => {
+    const date = new Date();
+    setTime({
+      hour: String(date.getHours()).padStart(2, '0'),
+      minute: String(date.getMinutes()).padStart(2, '0'),
+      second: String(date.getSeconds()).padStart(2, '0')
+    })
+  })
 
   return (
     <div className='App'>
       <Clock time={time}/>
     </div>
-  );
+  )
 }
 
 export default App;
